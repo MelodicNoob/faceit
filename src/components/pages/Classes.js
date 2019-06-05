6625f84c7c358f8174ba8c1d4cef4a895654eb0c
diskomo <carino.culetto@gmail.com>
2019-06-05 12:58:23 +0800

    some more classes

--- src/components/pages/Classes.js
+++ src/components/pages/Classes.js
@@ -7,7 +7,11 @@ export default class Classes extends Component {
   render() {
     const classes = [
       { id: 3102, name: 'Class 1' },
-      { id: 4183, name: 'Class 2' }
+      { id: 4183, name: 'Class 2' },
+      { id: 2009, name: 'Class 3' },
+      { id: 2103, name: 'Class 4' },
+      { id: 7782, name: 'Class 5' },
+      { id: 9298, name: 'Class 6' }
     ];
     return (
       <MDBCard color='red'>
--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
-import { MDBCard, MDBCardBody, MDBCardHeader } from 'mdbreact';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBCardUp,
+  MDBAvatar,
+  MDBRow,
+  MDBCol
+} from 'mdbreact';
 
 export default class Profile extends Component {
   render() {
@@ -11,7 +19,19 @@ export default class Profile extends Component {
             <i class='fal fa-user' /> Profile
           </h3>
         </MDBCardHeader>
-        <MDBCardBody />
+        <MDBCardBody>
+          <MDBRow>
+            <MDBCol md={3}>
+              <MDBAvatar className='white' circle>
+                <img
+                  src='https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg'
+                  alt=''
+                  className='rounded-circle'
+                />
+              </MDBAvatar>
+            </MDBCol>
+          </MDBRow>
+        </MDBCardBody>
       </MDBCard>
     );
   }
