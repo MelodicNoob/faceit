9f035f343273e71fbca27a03d9804497c1d55da9
diskomo <carino.culetto@gmail.com>
2019-06-06 15:27:58 +0800

    Added Pages component

--- src/App.js
+++ src/App.js
@@ -9,28 +9,57 @@ import Classes from './components/pages/Classes';
 import Schedule from './components/pages/Schedule';
 import Profile from './components/pages/Profile';
 import Settings from './components/pages/Settings';
+import Page from './components/pages/Page';
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<>
-					<div className='app-container'>
-						<Header />
-						<Switch>
-							<Route exact path='/' component={Login} />
-							<Route path='/dashboard' component={Dashboard} />
-							<Route path='/classes' component={Classes} />
-							<Route path='/schedule' component={Schedule} />
-							<Route path='/profile' component={Profile} />
-							<Route path='/settings' component={Settings} />
-						</Switch>
-					</div>
-					<Footer />
-				</>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <>
+          <div className='app-container'>
+            <Header />
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path='/dashboard' component={Dashboard} />
+              <Route
+                path='/classes'
+                render={() => (
+                  <Page title='Classes' icon='chalkboard-teacher' color='red'>
+                    <Classes />
+                  </Page>
+                )}
+              />
+              <Route
+                path='/schedule'
+                render={() => (
+                  <Page title='Schedule' icon='clock' color='purple'>
+                    <Schedule />
+                  </Page>
+                )}
+              />
+              <Route
+                path='/profile'
+                render={() => (
+                  <Page title='Profile' icon='user' color='orange'>
+                    <Profile />
+                  </Page>
+                )}
+              />
+              <Route
+                path='/settings'
+                render={() => (
+                  <Page title='Settings' icon='cog' color='blue'>
+                    <Settings />
+                  </Page>
+                )}
+              />
+            </Switch>
+          </div>
+          <Footer />
+        </>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/pages/Classes.js
+++ src/components/pages/Classes.js
@@ -1,54 +1,17 @@
 import React, { Component } from 'react';
-import { MDBCard, MDBCardHeader, MDBCardBody } from 'mdbreact';
 
 import ClassList from '../classes/ClassList';
 
 export default class Classes extends Component {
-	state = {
-		view: 'cards'
-	};
-
-	render() {
-		const listView = () => {
-			this.setState({ view: 'list' });
-		};
-		const cardsView = () => {
-			this.setState({ view: 'cards' });
-		};
-		const { view } = this.state;
-		const classes = [
-			{ id: 3102, name: 'Class 1' },
-			{ id: 4183, name: 'Class 2' },
-			{ id: 2009, name: 'Class 3' },
-			{ id: 2103, name: 'Class 4' },
-			{ id: 7782, name: 'Class 5' },
-			{ id: 9298, name: 'Class 6' }
-		];
-		return (
-			<MDBCard color='red'>
-				<MDBCardHeader className='text-white'>
-					<h3 className='float-left'>
-						<i className='fal fa-chalkboard-teacher' /> Classes
-					</h3>
-					<div className='view-selectors float-right'>
-						<i
-							onClick={listView}
-							className={
-								view === 'cards' ? 'fas fa-list' : 'fas fa-list active'
-							}
-						/>
-						<i
-							onClick={cardsView}
-							className={
-								view === 'cards' ? 'fas fa-th-large active' : 'fas fa-th-large'
-							}
-						/>
-					</div>
-				</MDBCardHeader>
-				<MDBCardBody>
-					<ClassList classes={classes} view={view} />
-				</MDBCardBody>
-			</MDBCard>
-		);
-	}
+  render() {
+    const classes = [
+      { id: 3102, name: 'Class 1' },
+      { id: 4183, name: 'Class 2' },
+      { id: 2009, name: 'Class 3' },
+      { id: 2103, name: 'Class 4' },
+      { id: 7782, name: 'Class 5' },
+      { id: 9298, name: 'Class 6' }
+    ];
+    return <ClassList classes={classes} />;
+  }
 }
