bbd95cf9d5b38beb15ed7219ce8c5e03d0237268
diskomo <carino.culetto@gmail.com>
2019-06-10 10:27:05 +0800

    fixed small href issue

--- src/App.js
+++ src/App.js
@@ -16,103 +16,108 @@ import Settings from './components/pages/panels/Settings';
 import Page from './components/pages/Page';
 
 const colors = {
-	red: '#FF0000',
-	purple: '#453192',
-	orange: '#fbb03b',
-	blue: '#29abe2'
+  red: '#FF0000',
+  purple: '#453192',
+  orange: '#fbb03b',
+  blue: '#29abe2'
 };
 
 const { red, purple, orange, blue } = colors;
 
 const pages = [
-	{
-		name: 'classes',
-		icon: 'chalkboard-teacher',
-		color: red
-	},
-	{
-		name: 'schedule',
-		icon: 'clock',
-		color: purple
-	},
-	{
-		name: 'profile',
-		icon: 'user',
-		color: orange
-	},
-	{
-		name: 'settings',
-		icon: 'cog',
-		color: blue
-	}
+  {
+    name: 'classes',
+    icon: 'chalkboard-teacher',
+    viewToggle: true,
+    color: red
+  },
+  {
+    name: 'schedule',
+    icon: 'clock',
+    viewToggle: false,
+    color: purple
+  },
+  {
+    name: 'profile',
+    icon: 'user',
+    viewToggle: false,
+    color: orange
+  },
+  {
+    name: 'settings',
+    icon: 'cog',
+    viewToggle: false,
+    color: blue
+  }
 ];
 
 const theme = createMuiTheme({
-	palette: {
-		type: 'light',
-		primary: {
-			main: red
-		}
-	}
+  palette: {
+    type: 'light',
+    primary: {
+      main: red
+    }
+  }
 });
 
 class App extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.changeTheme = theme => {
-			this.setState({ theme });
-		};
+    this.changeTheme = theme => {
+      this.setState({ theme });
+    };
 
-		this.state = {
-			theme
-		};
-	}
-	render() {
-		const { theme } = this.state;
-		return (
-			<Router>
-				<ThemeProvider theme={theme}>
-					<Box className='app-container'>
-						<Header changeTheme={this.changeTheme} />
-						<Switch>
-							<Route exact path='/' component={Login} />
-							<Route
-								path='/dashboard'
-								render={() => <Dashboard colors={colors} />}
-							/>
-							{pages.map(function({ name, icon }, i) {
-								return (
-									<Route
-										key={i}
-										path={`/${name}`}
-										render={() => (
-											<Page
-												title={name}
-												icon={icon}
-												color={theme.palette.primary.main}
-											>
-												{name === 'classes' ? (
-													<Classes />
-												) : name === 'schedule' ? (
-													<Schedule />
-												) : name === 'profile' ? (
-													<Profile />
-												) : name === 'settings' ? (
-													<Settings />
-												) : null}
-											</Page>
-										)}
-									/>
-								);
-							})}
-						</Switch>
-					</Box>
-					<Footer />
-				</ThemeProvider>
-			</Router>
-		);
-	}
+    this.state = {
+      theme
+    };
+  }
+  render() {
+    const { theme } = this.state;
+    return (
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Box className='app-container'>
+            <Header changeTheme={this.changeTheme} />
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route
+                path='/dashboard'
+                render={() => <Dashboard colors={colors} />}
+              />
+              {pages.map(function({ name, icon, viewToggle }, i) {
+                return (
+                  <Route
+                    key={i}
+                    path={`/${name}`}
+                    render={() => (
+                      <Page
+                        title={name}
+                        icon={icon}
+                        color={theme.palette.primary.main}
+                        viewToggle={viewToggle}
+                      >
+                        {name === 'classes' ? (
+                          <Classes />
+                        ) : name === 'schedule' ? (
+                          <Schedule />
+                        ) : name === 'profile' ? (
+                          <Profile />
+                        ) : name === 'settings' ? (
+                          <Settings />
+                        ) : null}
+                      </Page>
+                    )}
+                  />
+                );
+              })}
+            </Switch>
+          </Box>
+          <Footer />
+        </ThemeProvider>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/layout/Navigation/NavigationLink.js
+++ src/components/layout/Navigation/NavigationLink.js
@@ -3,37 +3,37 @@ import { NavLink } from 'react-router-dom';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const newTheme = main =>
-	createMuiTheme({
-		palette: {
-			primary: {
-				main
-			}
-		}
-	});
+  createMuiTheme({
+    palette: {
+      primary: {
+        main
+      }
+    }
+  });
 
 export default class NavigationLink extends Component {
-	render() {
-		const { isDash, changeTheme } = this.props;
-		const { name, linkColor, icon } = this.props.link;
-		return (
-			<li>
-				{isDash ? (
-					<NavLink
-						onClick={() => changeTheme(newTheme(linkColor))}
-						to={`/${name}`}
-						style={{ background: `${linkColor}` }}
-					>
-						<i className={`fa fa-${icon}`} /> {name}
-					</NavLink>
-				) : (
-					<NavLink
-						onClick={() => changeTheme(newTheme(linkColor))}
-						to={`/${name}`}
-					>
-						<i className={`fa fa-${icon}`} /> {name}
-					</NavLink>
-				)}
-			</li>
-		);
-	}
+  render() {
+    const { isDash, changeTheme } = this.props;
+    const { name, linkColor, icon } = this.props.link;
+    return (
+      <li>
+        {isDash ? (
+          <NavLink
+            onClick={() => changeTheme(newTheme(linkColor))}
+            to={`/${name}`}
+            style={{ background: `${linkColor}` }}
+          >
+            <i className={`fa fa-${icon}`} /> {name}
+          </NavLink>
+        ) : (
+          <NavLink
+            onClick={() => changeTheme(newTheme(linkColor))}
+            to={`/${name}`}
+          >
+            <i className={`fa fa-${icon}`} /> {name}
+          </NavLink>
+        )}
+      </li>
+    );
+  }
 }
