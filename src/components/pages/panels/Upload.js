67e375414c53a6412284731816c771e31cef8410
Jai Keery <carino.culetto@gmail.com>
2019-06-20 12:36:10 +0800

    Upload page adjustment

--- src/components/pages/panels/Upload.js
+++ src/components/pages/panels/Upload.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 const Upload = () => {
 	return (
 		<Grid container spacing={4}>
 			<Grid item xs={12} md={6} lg={4}>
 				<Card>Upload</Card>
+				<form autoFill='off'>
+					<TextField label='Class Name' />
+				</form>
 			</Grid>
 		</Grid>
 	);
