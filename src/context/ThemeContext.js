736a5a1b33514a928b834bdae47769b562717a61
Jai Keery <carino.culetto@gmail.com>
2019-06-22 13:08:40 +0800

    Added ThemeContext

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { createMuiTheme } from '@material-ui/core';
 
-const colors = {
+export const colors = {
 	red: '#FF0000',
 	purple: '#453192',
 	orange: '#fbb03b',
 	blue: '#29abe2'
 };
 
-export const theme = {
+const defaultTheme = createMuiTheme({
 	palette: {
 		primary: {
 			main: colors.red
 		}
 	}
-};
+});
 
-export const newTheme = main =>
-	createMuiTheme({
-		palette: {
-			primary: {
-				main
-			}
-		}
-	});
-
-export const ThemeContext = React.createContext({
-	theme,
+const ThemeContext = React.createContext({
+	theme: defaultTheme,
 	changeTheme: () => {}
 });
+
+export default ThemeContext;
+
+export function ThemeProvider(props) {
+	const [theme, setTheme] = useState(defaultTheme);
+	const changeTheme = color => {
+		setTheme(
+			createMuiTheme({
+				palette: {
+					primary: {
+						main: color
+					}
+				}
+			})
+		);
+	};
+	return (
+		<ThemeContext.Provider
+			value={{
+				theme,
+				changeTheme
+			}}
+		>
+			{props.children}
+		</ThemeContext.Provider>
+	);
+}
